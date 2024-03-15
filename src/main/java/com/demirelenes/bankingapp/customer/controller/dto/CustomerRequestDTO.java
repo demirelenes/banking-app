62f267234caa0e4e5c2702019d111d6500622f2c
Enes Demirel <enesdemirel20@hotmail.com>
2024-03-15 17:54:50 +0300

    Add DTOs for customer

--- src/main/java/com/demirelenes/bankingapp/customer/controller/dto/CustomerRequestDTO.java
+++ src/main/java/com/demirelenes/bankingapp/customer/controller/dto/CustomerRequestDTO.java
@@ -1,4 +1,11 @@
 package com.demirelenes.bankingapp.customer.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class CustomerRequestDTO {
+
+    private String firstName;
+
+    private String lastName;
 }
--- src/main/java/com/demirelenes/bankingapp/customer/controller/dto/CustomerResponseDTO.java
+++ src/main/java/com/demirelenes/bankingapp/customer/controller/dto/CustomerResponseDTO.java
@@ -1,4 +1,13 @@
 package com.demirelenes.bankingapp.customer.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class CustomerResponseDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
 }
